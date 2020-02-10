771d4f4abef8b47f2907658e94f17e429787d97c
khwan <rudghks7816@gmail.com>
2020-02-10 17:24:37 +0900

    feat: pc 헤더 추가

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,22 @@
 <template>
   <div id="app">
+    <c-header />
+
     <div class="container">
       <router-view />
     </div>
   </div>
 </template>
+
+<script>
+import cHeader from './components/base/cHeader'
+
+export default {
+  components: {
+    cHeader
+  }
+}
+</script>
 
 <style lang="scss">
 @import './assets/scss/bootstrap/custom.scss';
---
+++ src/components/base/cHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <div class="container">
+      <h1 class="title">[제출일] 이경환</h1>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  align-items: center;
+  height: 70px;
+  box-shadow: 0 1px 5px 0 rgba(0, 0, 0, 0.07);
+}
+.title {
+  font-size: 1rem;
+}
+</style>
